f7cfa9edb8255fd0cd99724ce7aed0d1358e208d
aboelenien <mohamed.aboelenien@thewebops.com>
2019-11-08 19:25:41 +0200

    removed messages count from messages

--- db/migrate/20191107205946_create_messages.rb
+++ db/migrate/20191107205946_create_messages.rb
@@ -3,7 +3,6 @@ class CreateMessages < ActiveRecord::Migration[5.2]
     create_table :messages do |t|
       t.text :message
       t.bigint :number
-      t.bigint :messages_count
       t.references :chat, foreign_key: true
 
       t.timestamps
