94cc15bad66acb75c02d3d4f87bcb86383f5bc7f
aboelenien <mohamed.aboelenien@thewebops.com>
2019-11-08 17:00:40 +0200

    elastic search added to messages

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,26 @@
 class Message < ApplicationRecord
   belongs_to :chat,  dependent: :destroy
+  before_save :force_index
+
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
+
+  settings do
+     mappings dynamic: false do
+       indexes :message, type: :text
+     end
+  end
+
+  def as_indexed_json(options={})
+     self.as_json(root: false, only: [:number, :message])
+  end
+
+  def self.serach(query)
+   __elasticsearch__.search(query)
+  end
+
+  def force_index
+    __elasticsearch__.instance_variable_set(:@__changed_model_attributes, nil)
+  end
+
 end
